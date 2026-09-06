713a4539cc41cc64141d356e936a71d471e23fd4
soenneker <jake@soenneker.com>
2026-09-06 13:53:33 -0500

    Update dependencies and fix build

--- test/Soenneker.Extensions.CultureInfos.Tests/CultureInfosExtensionTests.cs
+++ test/Soenneker.Extensions.CultureInfos.Tests/CultureInfosExtensionTests.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Threading.Tasks;
 using Soenneker.Tests.Unit;
 
 namespace Soenneker.Extensions.CultureInfos.Tests;
@@ -9,7 +8,7 @@ namespace Soenneker.Extensions.CultureInfos.Tests;
 public sealed class CultureInfosExtensionTests : UnitTest
 {
     [Test]
-    public async Task GetWeekendDays_does_not_expose_mutable_shared_hashset()
+    public async global::System.Threading.Tasks.Task GetWeekendDays_does_not_expose_mutable_shared_hashset()
     {
         IReadOnlySet<DayOfWeek> days = CultureInfo.GetCultureInfo("en-US").GetWeekendDays();
 
@@ -19,7 +18,7 @@ public sealed class CultureInfosExtensionTests : UnitTest
     }
 
     [Test]
-    public async Task Arabic_culture_uses_friday_saturday_pattern()
+    public async global::System.Threading.Tasks.Task Arabic_culture_uses_friday_saturday_pattern()
     {
         CultureInfo culture = CultureInfo.GetCultureInfo("ar-SA");
 
